8c193942f5654c2162996d63cc15b02215e3ddd3
vpayne-neo <vince.payne@neofinancial.com>
2023-03-23 12:09:27 -0600

    Added a script to read my ts file for testing

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
+  console.log('hello')
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
