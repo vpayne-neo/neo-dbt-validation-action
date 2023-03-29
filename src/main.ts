60a77e9291f9e085eaf8b6708035832e7e6dce7b
vpayne-neo <vince.payne@neofinancial.com>
2023-03-29 15:52:41 -0600

    fixing error handling

--- src/main.ts
+++ src/main.ts
@@ -9,67 +9,75 @@ import parseDbtAsNativeSql from './parseDbtasNativeSql'
 import {getInput} from '@actions/core'
 
 async function run(): Promise<void> {
-  const paths = getInput('files')
-  core.debug(paths)
-  const sqlFilePaths = paths.split(' ').filter(sql => sql.includes('.sql'))
+  try {
+    const paths = getInput('files')
+    core.debug(paths)
+    const sqlFilePaths = paths.split(' ').filter(sql => sql.includes('.sql'))
 
-  const ymlFilePaths = paths.split(' ').filter(yml => yml.includes('.yml'))
+    const ymlFilePaths = paths.split(' ').filter(yml => yml.includes('.yml'))
 
-  type SqlYmlFilePairs = {
-    sqlAsString: string
-    ymlFilePath: string
+    type SqlYmlFilePairs = {
+      sqlAsString: string
+      ymlFilePath: string
+    }
+
+    const filePairs: Array<SqlYmlFilePairs> = sqlFilePaths.map(sqlFile => {
+      const yml = ymlFilePaths.find(yml =>
+        yml.includes(sqlFile.replace('.sql', ''))
+      )
+
+      return {
+        sqlAsString: fs.readFileSync(sqlFile, 'utf-8'),
+        ymlFilePath: yml ?? ''
+      }
+    })
+
+    filePairs.map(async pair => {
+      const parser = new Parser()
+
+      const sqlToObject = parser.astify(parseDbtAsNativeSql(pair.sqlAsString))
+      const columnNames = sqlToObject.columns
+        .map(
+          (col: {
+            expr: {
+              type?: string
+              table?: string
+              column?: string
+            }
+            as?: string
+          }) => `${col.as ?? col.expr.column}`
+        )
+        .sort()
+
+      const ymlColumnNames = await getYmlDetails(pair.ymlFilePath)
+      console.log(pair)
+      const ymlColumnCount = ymlColumnNames.length
+      const sqlColumnCount = columnNames.length
+      console.log(
+        ` Column names equal? : ${isDeepStrictEqual(
+          ymlColumnNames,
+          columnNames
+        )}`
+      )
+      if (isDeepStrictEqual(ymlColumnNames, columnNames) == false) {
+        const difference = differenceBy(columnNames, ymlColumnNames).map(
+          diff => ` ${diff}`
+        )
+        const errorMsg = `Columns do not match =>> ${difference}`
+        core.error(errorMsg)
+      }
+
+      throw new Error(
+        ` Column count equal? : ${isDeepStrictEqual(
+          ymlColumnCount,
+          sqlColumnCount
+        )}`
+      )
+    })
+  } catch (err) {
+    console.error(err)
+    process.exit()
   }
-
-  const filePairs: Array<SqlYmlFilePairs> = sqlFilePaths.map(sqlFile => {
-    const yml = ymlFilePaths.find(yml =>
-      yml.includes(sqlFile.replace('.sql', ''))
-    )
-
-    return {
-      sqlAsString: fs.readFileSync(sqlFile, 'utf-8'),
-      ymlFilePath: yml ?? ''
-    }
-  })
-
-  filePairs.map(async pair => {
-    const parser = new Parser()
-
-    const sqlToObject = parser.astify(parseDbtAsNativeSql(pair.sqlAsString))
-    const columnNames = sqlToObject.columns
-      .map(
-        (col: {
-          expr: {
-            type?: string
-            table?: string
-            column?: string
-          }
-          as?: string
-        }) => `${col.as ?? col.expr.column}`
-      )
-      .sort()
-
-    const ymlColumnNames = await getYmlDetails(pair.ymlFilePath)
-    console.log(pair)
-    const ymlColumnCount = ymlColumnNames.length
-    const sqlColumnCount = columnNames.length
-    console.log(
-      ` Column names equal? : ${isDeepStrictEqual(ymlColumnNames, columnNames)}`
-    )
-    if (isDeepStrictEqual(ymlColumnNames, columnNames) == false) {
-      const difference = differenceBy(columnNames, ymlColumnNames).map(
-        diff => ` ${diff}`
-      )
-      const errorMsg = `Columns do not match =>> ${difference}`
-      core.error(errorMsg)
-    }
-
-    core.debug(
-      ` Column count equal? : ${isDeepStrictEqual(
-        ymlColumnCount,
-        sqlColumnCount
-      )}`
-    )
-  })
 }
 
 run()
