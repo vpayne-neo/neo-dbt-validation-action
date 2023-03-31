7b75545053411db424b274f07c619c1939f696e7
vpayne-neo <vince.payne@neofinancial.com>
2023-03-31 11:48:16 -0600

    New Prod build

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ import {getInput} from '@actions/core'
 async function run(): Promise<void> {
   try {
     const paths = getInput('files')
-
     core.debug(paths)
-    const sqlFilePaths = paths.split(' ').filter(sql => sql.includes('.sql'))
 
+    const sqlFilePaths = paths.split(' ').filter(sql => sql.includes('.sql'))
     const ymlFilePaths = paths.split(' ').filter(yml => yml.includes('.yml'))
 
     type SqlYmlFilePairs = {
@@ -25,7 +24,6 @@ async function run(): Promise<void> {
       const yml = ymlFilePaths.find(yml =>
         yml.includes(sqlFile.replace('.sql', ''))
       )
-
       return {
         sqlAsString: fs.readFileSync(sqlFile, 'utf-8'),
         ymlFilePath: yml ?? ''
@@ -34,7 +32,7 @@ async function run(): Promise<void> {
 
     filePairs.map(async pair => {
       const parser = new Parser()
-      parseDbtAsNativeSql(pair.sqlAsString)
+
       const sqlToObject = parser.astify(parseDbtAsNativeSql(pair.sqlAsString))
       const columnNames = sqlToObject.columns
         .map(
@@ -51,6 +49,7 @@ async function run(): Promise<void> {
 
       const ymlColumnNames = await getYmlDetails(pair.ymlFilePath)
       core.debug(`${pair.sqlAsString} \n ${pair.ymlFilePath}`)
+
       const ymlColumnCount = ymlColumnNames.length
       const sqlColumnCount = columnNames.length
       core.debug(
--- src/parseDbtasNativeSql.ts
+++ src/parseDbtasNativeSql.ts
@@ -1,3 +1,5 @@
+import {differenceBy} from 'lodash'
+
 const parseDbtAsNativeSql = (dbtSQL: string): string => {
   // This funtion reads a string and removes dbt patterns from it
   let sql = dbtSQL
@@ -11,6 +13,7 @@ const parseDbtAsNativeSql = (dbtSQL: string): string => {
   if (cteCount > 1 && cteCount !== 0) {
     for (let i = 0; i < cteCount; i++) {
       sql = sql.replace(/\sas\s/, ' as').replace(/\n/g, ' ')
+
       // remove space after as for each cte and places string on one line
       const matchedCte = sql.match(/as\(.*?[^\)]from/)?.map(cte => cte)[0] // assigns current cte to variable
 
@@ -20,23 +23,32 @@ const parseDbtAsNativeSql = (dbtSQL: string): string => {
         // on the last cte we assign out matched cte to the sql variable
         sql = matchedCte ?? ''
 
-        sql = sql?.replace('as(', '').replace('from ', '')
+        sql = sql?.replace('as(', '')
+
+        const columnJinja = sql?.match(/{{.*?[^]}}\sas/g)
+
+        columnJinja?.map(jinja => (sql = sql?.replace(jinja, '')))
+
+        sql = sql.concat(' ') //adds white space to end of string to make sure final "from " is matched
+        sql = sql.replace('from ', '')
 
         return sql
       }
     }
   } else {
-    sql = sql.replace(/\sas\s/, ' as').replace(/\n/g, ' ')
-    // remove space after as
+    sql = sql.replace(/\sas\s/, ' as').replace(/\n/g, ' ') // remove space after as and formats string on one line
     const selectStatement = sql.match(/as\(.*?[^\)]from\s/) //matches everythin between as( - from
 
-    const selectWithoutAs = selectStatement // removes string from regex array and store in variable, then removes as(
+    const selectWithoutAs = selectStatement
       ?.map(select => select)[0]
-      ?.replace('as(', '')
+      ?.replace('as(', '') // removes string from regex array and store in variable, then removes as(
 
-    const removeFrom = selectWithoutAs?.replace('from ', '') //removes 'from' from the string
+    sql = selectWithoutAs ?? ''
 
-    sql = removeFrom ?? ''
+    const columnJinja = sql?.match(/{{.*?[^]}}\sas/g) // finds all occurences of column jinja and removes it while leaving column name
+    columnJinja?.map(jinja => (sql = sql?.replace(jinja, '')))
+
+    sql = sql?.replace('from ', '') //removes 'from' from the string
     return sql
   }
 
